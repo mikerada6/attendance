d4d87bf1eb01b4e93f53a13c70a6e51ed7710bbd
mradaszkiewicz <mrada6@gmail.com>
2018-10-11 11:41:32 -0400

    temp

--- welcome.py
+++ welcome.py
@@ -26,6 +26,7 @@ def signIn(channel):
         display.lcd_display_string_right(timestamp, 1)
         display.lcd_display_string("Sign In", 3)
         display.lcd_display_string("Please swipe card now", 4)
+        str=raw_input()
         time.sleep(.95)
 
 
@@ -47,15 +48,16 @@ channel=37
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(channel, GPIO.FALLING, callback=signIn, bouncetime=300)
 try:
-    while contMainMenu:
-        currentDT = datetime.datetime.now()
-        timestamp = str(currentDT.month)+"/"+str(str(currentDT.day))+"/"+str(currentDT.year)+"  " + format(currentDT.hour%12, '02d')+":"+format(currentDT.minute, '02d')+":"+format(currentDT.second, '02d')
-        display.lcd_display_string_right(timestamp, 1)
-        display.lcd_display_string(teacher + "-" + room, 2)
-        buttons1 = ["Sign","","","Sign"]
-        buttons2 = ["Out","","","In"]
-        display.lcd_display_string_buttons(buttons1,3)
-        display.lcd_display_string_buttons(buttons2,4)
+    while True:
+        while contMainMenu:
+            currentDT = datetime.datetime.now()
+            timestamp = str(currentDT.month)+"/"+str(str(currentDT.day))+"/"+str(currentDT.year)+"  " + format(currentDT.hour%12, '02d')+":"+format(currentDT.minute, '02d')+":"+format(currentDT.second, '02d')
+            display.lcd_display_string_right(timestamp, 1)
+            display.lcd_display_string(teacher + "-" + room, 2)
+            buttons1 = ["Sign","","","Sign"]
+            buttons2 = ["Out","","","In"]
+            display.lcd_display_string_buttons(buttons1,3)
+            display.lcd_display_string_buttons(buttons2,4)
         time.sleep(.95)
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
     print("Cleaning up!")
