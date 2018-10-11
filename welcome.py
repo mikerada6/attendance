5fcaacf520f0c6331a7639a2ac5c579f1cc242ea
Michael <mrada6@gmail.com>
2018-10-10 23:02:29 -0400

    added a clock that updates every second

--- welcome.py
+++ welcome.py
@@ -11,14 +11,16 @@ exists = os.path.isfile('/path/to/file')
 if not exists:
     import configWriter
 try:
-    currentDT = datetime.datetime.now()
-    timestamp = str(currentDT.month)+"/"+str(str(currentDT.day))+"/"+str(currentDT.year)+"  " + str(currentDT.hour)+":"+str(currentDT.minute)
-    display.lcd_display_string(timestamp, 1)
-    buttons1 = ["Sign","","","Sign"]
-    buttons2 = ["Out","","","In"]
-    display.lcd_display_string_buttons(buttons1,3)
-    display.lcd_display_string_buttons(buttons2,4)
-    time.sleep(600)
+    while True:
+        currentDT = datetime.datetime.now()
+        timestamp = str(currentDT.month)+"/"+str(str(currentDT.day))+"/"+str(currentDT.year)+"  " + str(currentDT.hour)+":"+str(currentDT.minute)+":"+str(currentDT.second)
+        display.lcd_display_string_right(timestamp, 1)
+        display.lcd_display_string("What would you like to do?", 1)
+        buttons1 = ["Sign","","","Sign"]
+        buttons2 = ["Out","","","In"]
+        display.lcd_display_string_buttons(buttons1,3)
+        display.lcd_display_string_buttons(buttons2,4)
+        time.sleep(.95)
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
     print("Cleaning up!")
 finally:
