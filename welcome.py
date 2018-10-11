48e2a01609a8565ed718312dcd8eadd274d32de8
Michael <mrada6@gmail.com>
2018-10-10 20:56:35 -0400

    Testing a new cleanup as a finally

--- welcome.py
+++ welcome.py
@@ -8,4 +8,5 @@ try:
     display.lcd_display_string("No time to waste", 1)
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
     print("Cleaning up!")
+finally:
     display.lcd_clear()
