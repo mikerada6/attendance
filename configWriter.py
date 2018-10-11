70a2a072f56ba89e15ff20cb9564cbd2a02cb55a
mradaszkiewicz <mrada6@gmail.com>
2018-10-11 12:00:17 -0400

    Storing the username and cleartext password in the config file.  This MUST be changed

--- configWriter.py
+++ configWriter.py
@@ -4,12 +4,12 @@ import os
 config = configparser.ConfigParser()
 # lets create that config file for next time...
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-dir_path = os.path.dirname(os.path.realpath(__file__))
 cfgfile = open(DIR_PATH + "/" + "attendance.ini", 'w')
 
 # add the settings to the structure of the file, and lets write it out...
 config["SOFTWARE"] = {"Author": "M.Radaszkiewcz", "Version": "0.0.1", "Date": "2018.10.10"}
 config["ROOM"] = {"Room": "206", "Teacher": "mradaszkiewicz", "TecherId": "1", "School": "Trenton Catholic Academy",
                   "SchoolId": "1"}
+config["PI"] ={"Password":"D2E5C815E2312784454BCD43BA326","Id":"8218945314"}
 with open('attendance.ini', 'w') as configfile:
     config.write(configfile)
