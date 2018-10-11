5fd9b542811493beae713cd2b21685c131ea71cb
Michael <mrada6@gmail.com>
2018-10-10 22:52:15 -0400

    redid the config file to be made and read with python3

--- configWriter.py
+++ configWriter.py
@@ -1,21 +1,13 @@
 import  configparser
 import os
-Config =  configparser.ConfigParser()
+config = configparser.ConfigParser()
 # lets create that config file for next time...
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cfgfile = open(DIR_PATH+"/"+"attendance.ini",'w')
 
 # add the settings to the structure of the file, and lets write it out...
-Config.add_section('Software')
-Config.set('Software','Author',"M.Radaszkiewcz")
-Config.set('Software','Version',"0.01")
-Config.set('Software','Date',"2018.10.10")
-Config.add_section('Room')
-Config.set('Room','Number',206)
-Config.set('Room','Teacher', "mradaszkiewicz")
-Config.set('Room','TeacherID', "1")
-Config.set('Room','School', "TCA")
-Config.set('Room','SchoolID', "1")
-Config.write(cfgfile)
-cfgfile.close()
+config["SOFTWARE"]={"Author","M.Radaszkiewcz","Version","0.0.1","Date","2018.10.10"}
+config["ROOM"]={"Room","206","Teacher","mradaszkiewicz","TecherId","1","School","Trenton Catholic Academy","SchoolId","1"}
+with open('attendance.ini', 'w') as configfile:
+    config.write(configfile)
