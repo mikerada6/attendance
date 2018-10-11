38336d81d7ba46b845f14ed880e23224d2b9bcdc
Michael <mrada6@gmail.com>
2018-10-10 23:08:37 -0400

    Adding teacher's name and room number

--- welcome.py
+++ welcome.py
@@ -2,6 +2,7 @@ import lcddriver
 import time
 import datetime
 import os
+import configparser
 
 display = lcddriver.lcd()
 print("Writing to display")
@@ -10,12 +11,17 @@ cfgfile = DIR_PATH+"/"+"attendance.ini"
 exists = os.path.isfile('/path/to/file')
 if not exists:
     import configWriter
+config = configparser.ConfigParser()
+config.sections()
+config.read('attendance.ini')
+teacher = config['ROOM']['Teacher']
+room = config['ROOM']['Room']
 try:
     while True:
         currentDT = datetime.datetime.now()
         timestamp = str(currentDT.month)+"/"+str(str(currentDT.day))+"/"+str(currentDT.year)+"  " + str(currentDT.hour)+":"+str(currentDT.minute)+":"+str(currentDT.second)
         display.lcd_display_string_right(timestamp, 1)
-        display.lcd_display_string("What would you like to do?", 2)
+        display.lcd_display_string(teacher + "-" + room, 2)
         buttons1 = ["Sign","","","Sign"]
         buttons2 = ["Out","","","In"]
         display.lcd_display_string_buttons(buttons1,3)
