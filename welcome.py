71de81b81419c127e96b6b1458bb464c0d14bf34
Michael <mrada6@gmail.com>
2018-10-10 21:23:00 -0400

    testing right justify

--- welcome.py
+++ welcome.py
@@ -6,7 +6,7 @@ display = lcddriver.lcd()
 print("Writing to display")
 try:
     display.lcd_display_string("No time to waste", 1)
-    display.lcd_display_string_right("gotta go right now",2)
+    display.lcd_display_string_right("abcdefghijklmnopqrstuvwxyz",2)
     time.sleep(5)
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
     print("Cleaning up!")
